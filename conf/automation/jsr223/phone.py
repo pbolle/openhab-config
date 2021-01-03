5cf23dd08936d84be0cc1bcf4b6e0b52fb4f9330
Jan-Philipp Bolle <pbolle@gmx.de>
2021-01-03 16:55:43 +0100

    sip umweg

--- conf/automation/jsr223/phone.py
+++ conf/automation/jsr223/phone.py
@@ -12,6 +12,6 @@ class DoorBellNotificationRule:
     def execute(self, module, input):
         state = input['event'].getItemState()
 #        self.log.info(u"type {}".format(type(state)))
-        if itemStateOlderThen("Gardendoor_Bell_Last_Change", getNow().minusSeconds(30)) and type(state) is StringListType and state.getValue(1) == '017644424292' :
+        if itemStateOlderThen("Gardendoor_Bell_Last_Change", getNow().minusSeconds(30)) and type(state) is StringListType and state.getValue(1) == '4933762979088@192.168.178.1' :
             sendNotification("Klingel", "Es klingelt", "https://bolle.haus/cameraStrasseImage" )
         postUpdate("Gardendoor_Bell_Last_Change", DateTimeType())
