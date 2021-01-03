02ec39d2f123ab359673199ecc9b5c5a3d350453
Jan-Philipp Bolle <pbolle@gmx.de>
2021-01-03 15:04:31 +0100

    check doorbell

--- conf/automation/jsr223/phone.py
+++ conf/automation/jsr223/phone.py
@@ -1,32 +1,17 @@
 from shared.helper import rule, getNow, itemStateOlderThen, sendNotification, postUpdate
 from core.triggers import ItemStateChangeTrigger, CronTrigger
+from org.eclipse.smarthome.core.library.types import StringListType
 
 @rule("phone.py")
 class DoorBellNotificationRule:
     def __init__(self):
         self.triggers = [
-#            CronTrigger("15 * * * * ?"),
-#            ItemStateChangeTrigger("IncomingCall", state="OPEN")
             ItemStateChangeTrigger("IncomingCall")
         ]
 
     def execute(self, module, input):
         state = input['event'].getItemState()
-        self.log.info(u"telefon {} {}".format(state.oldState))
-
-#        self.log.info(u"test {}".format(input))
-#        self.log.info(u"test {}".format(module))
-
-#        state = input['event'].getItemState()
-#        name = input['event'].getItemName()
-
-#        self.log.info(u"test {} {}".format(type(state), name))
-
-        #if itemStateOlderThen("pOutdoor_Streedside_Gardendoor_Bell_Last_Change", getNow().minusSeconds(30)):
-        #    sendNotification("Klingel", "Es klingelt", "https://smartmarvin.de/cameraStrasseImage" )
-
-        #postUpdate("pOutdoor_Streedside_Gardendoor_Bell_Last_Change", DateTimeType())
-
-
-
-
+#        self.log.info(u"type {}".format(type(state)))
+        if itemStateOlderThen("Gardendoor_Bell_Last_Change", getNow().minusSeconds(30)) and type(state) is StringListType and state.getValue(1) == '017644424292' :
+            sendNotification("Klingel", "Es klingelt", "https://bolle.haus/cameraStrasseImage" )
+        postUpdate("Gardendoor_Bell_Last_Change", DateTimeType())
